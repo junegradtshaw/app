f60f4c529bc3a477be11462639c35626db2a1a39
June Shaw <june.shaw@me.com>
2016-02-23 13:54:27 -0700

    multiple routes

--- js/controllers/main_controller.js
+++ js/controllers/main_controller.js
@@ -1,3 +1,3 @@
-app.controller('MainController', ['scope', function($scope) {
-  $scope.works = "hello";
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.whereami = "main";
 }])
